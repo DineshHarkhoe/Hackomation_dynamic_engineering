026c93d2abb1b7bcdb7257db268373861a058848
dinesh <dinharkhoe@gmail.com>
2022-04-04 16:49:06 -0300

    hcsro4

--- main/Hackomation_v1.c
+++ main/Hackomation_v1.c
@@ -10,9 +10,12 @@
 #include "esp_err.h"
 #include "esp_task_wdt.h"
 #include "esp_log.h"
+#include "esp_check.h"
 
 #include "driver/i2c.h"
 #include "driver/mcpwm.h"
+
+#include "soc/rtc.h"
 
 #include "../components/ahrs/MadgwickAHRS.h"
 #include "../components/mpu9250/mpu9250.h"
@@ -29,7 +32,24 @@ static const char *TAG = "Drone";
 #define BACK_LEFT_CCW_GPIO 10
 #define FRONT_LEFT_CW_GPIO 2
 #define FRONT_RIGHT_CCW_GPIO 4
-#define I2C_MASTER_NUM 0 /*!< I2C port number for master dev */
+#define I2C_MASTER_NUM 0
+
+#define HC_SR04_SAMPLE_PERIOD_MS    100
+#define HC_SR04_PIN_ECHO    GPIO_NUM_32
+#define HC_SR04_PIN_TRIG    GPIO_NUM_33
+#define TRIGGER_THREAD_STACK_SIZE 512
+#define TRIGGER_THREAD_PRIORITY 5
+_Static_assert(HC_SR04_SAMPLE_PERIOD_MS > 50, "Sample period too short!");
+
+typedef struct {
+    uint32_t capture_signal;
+    mcpwm_capture_signal_t sel_cap_signal;
+} capture;
+
+static uint32_t cap_val_begin_of_sample = 0;
+static uint32_t cap_val_end_of_sample = 0;
+
+static QueueHandle_t cap_queue;
 
 calibration_t cal = {
     .mag_offset = {.x = -31594.468750, .y = 5600.710938, .z = -4488.644531},
@@ -98,6 +118,89 @@ void get_mpu9265(float *heading, float *pitch, float *roll, float *temp){
     MadgwickGetEulerAnglesDegrees(heading, pitch, roll);
 }
 
+/**
+ * generate single pulse on Trig pin to activate a new sample
+ */
+static void gen_trig_output(void *arg) {
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+    while (true) {
+        vTaskDelayUntil(&xLastWakeTime, HC_SR04_SAMPLE_PERIOD_MS / portTICK_PERIOD_MS);
+        ESP_ERROR_CHECK(gpio_set_level(HC_SR04_PIN_TRIG, 1)); // set high
+        esp_rom_delay_us(10);
+        ESP_ERROR_CHECK(gpio_set_level(HC_SR04_PIN_TRIG, 0)); // set low
+    }
+}
+
+/**
+ * this is an ISR callback, we take action according to the captured edge
+ */
+static bool sr04_echo_isr_handler(mcpwm_unit_t mcpwm, mcpwm_capture_channel_id_t cap_sig, const cap_event_data_t *edata,void *arg) {
+    //calculate the interval in the ISR,
+    //so that the interval will be always correct even when cap_queue is not handled in time and overflow.
+    BaseType_t high_task_wakeup = pdFALSE;
+    if (edata->cap_edge == MCPWM_POS_EDGE) {
+        // store the timestamp when pos edge is detected
+        cap_val_begin_of_sample = edata->cap_value;
+        cap_val_end_of_sample = cap_val_begin_of_sample;
+    } else {
+        cap_val_end_of_sample = edata->cap_value;
+        // following formula refers to: https://www.elecrow.com/download/HC_SR04%20Datasheet.pdf
+        uint32_t pulse_count = cap_val_end_of_sample - cap_val_begin_of_sample;
+        // send measurement back though queue
+        xQueueSendFromISR(cap_queue, &pulse_count, &high_task_wakeup);
+    }
+    return high_task_wakeup == pdTRUE;
+}
+
+void get_hcsr04(float *distance){
+    uint32_t pulse_count;
+    // block and wait for new measurement
+    xQueueReceive(cap_queue, &pulse_count, portMAX_DELAY);
+    uint32_t pulse_width_us = pulse_count * (1000000.0 / rtc_clk_apb_freq_get());
+    // following formula is based on: https://www.elecrow.com/download/HC_SR04%20Datasheet.pdf
+    if (pulse_width_us > 35000) {
+    }
+    *distance = (float) pulse_width_us / 58;
+}
+
+void hcsr04_setup(){
+    // the queue where we read data
+    cap_queue = xQueueCreate(1, sizeof(uint32_t));
+    if (cap_queue == NULL) {
+        ESP_LOGE(TAG, "failed to alloc cap_queue");
+        return;
+    }
+
+    /* configure Echo pin */
+    // set CAP_0 on GPIO
+    ESP_ERROR_CHECK(mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM_CAP_0, HC_SR04_PIN_ECHO));
+    // enable pull down CAP0, to reduce noise
+    ESP_ERROR_CHECK(gpio_pulldown_en(HC_SR04_PIN_ECHO));
+    // enable both edge capture on CAP0
+    mcpwm_capture_config_t conf = {
+        .cap_edge = MCPWM_BOTH_EDGE,
+        .cap_prescale = 1,
+        .capture_cb = sr04_echo_isr_handler,
+        .user_data = NULL
+    };
+    ESP_ERROR_CHECK(mcpwm_capture_enable_channel(MCPWM_UNIT_0, MCPWM_SELECT_CAP0, &conf));
+    ESP_LOGI(TAG, "Echo pin configured");
+
+    /* configure Trig pin */
+    gpio_config_t io_conf = {
+        .intr_type = GPIO_INTR_DISABLE,
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pin_bit_mask = BIT64(HC_SR04_PIN_TRIG),
+    };
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
+    ESP_ERROR_CHECK(gpio_set_level(HC_SR04_PIN_TRIG, 0)); // drive low by default
+    ESP_LOGI(TAG, "Trig pin configured");
+
+    xTaskCreate(gen_trig_output, "gen_trig_output", TRIGGER_THREAD_STACK_SIZE, NULL, TRIGGER_THREAD_PRIORITY, NULL);
+}
+
 void mcpwm_setup(){
     mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, FRONT_LEFT_CW_GPIO);
     mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM1A, FRONT_RIGHT_CCW_GPIO);
@@ -136,10 +239,12 @@ void set_bldc_speed(int fr, int fl, int br, int bl){
 void app_main(void)
 {
     mcpwm_setup();
+    hcsr04_setup();
     i2c_mpu9250_init(&cal);
     MadgwickAHRSinit(SAMPLE_FREQ_Hz, 0.8);
 
-    float heading, pitch, roll, temp;
+    float heading, pitch, roll, temp, alt;
+    get_hcsr04(&alt);
     get_mpu9265(&heading, &pitch, &roll, &temp);
-    ESP_LOGI(TAG, "heading: %2.3f°, pitch: %2.3f°, roll: %2.3f°, Temp %2.3f°C", heading, pitch, roll, temp);
+    ESP_LOGI(TAG, "heading: %2.3f°, pitch: %2.3f°, roll: %2.3f°, Temp %2.3f°C, height: %2.3fcm", heading, pitch, roll, temp, alt);
 }
