c5ee2ec6ccb54075ab057367312a28a8c3b242b8
dinesh <dinharkhoe@gmail.com>
2022-05-19 18:30:21 -0300

    initial pid control

--- main/Hackomation_v1.c
+++ main/Hackomation_v1.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+
 #include "soc/rtc.h"
 #include "nvs_flash.h"
 #include "protocol_examples_common.h"
@@ -21,6 +22,7 @@
 
 #include "driver/i2c.h"
 #include "driver/mcpwm.h"
+#include "driver/gpio.h"
 
 #include "../components/ahrs/MadgwickAHRS.h"
 #include "../components/mpu9250/mpu9250.h"
@@ -29,28 +31,46 @@
 #include "../components/mpu9250/i2c-easy.h"
 #include "../components/mpu9250/bmp280.h"
 #include "../components/nmea/nmea_parser.h"
+#include "../components/pid/pid_ctrl.h"
 
 static const char *TAG = "Drone";
 
-#define BACK_RIGHT_CW_GPIO          9
-#define BACK_LEFT_CCW_GPIO          10
+//15    FR      //1153
+//12    BL      //1140
+//13    BR      //1140
+//2     FL      //1011
+
+#define MOTOR_OFFSET_FL             128
+#define MOTOR_OFFSET_FR             130
+#define MOTOR_OFFSET_BL             1
+#define MOTOR_OFFSET_BR             143
+
 #define FRONT_LEFT_CW_GPIO          2
-#define FRONT_RIGHT_CCW_GPIO        4
+#define FRONT_RIGHT_CCW_GPIO        15
+#define BACK_LEFT_CCW_GPIO          12
+#define BACK_RIGHT_CW_GPIO          13
+
+#define TXD_PIN_APPLICATION         27
+#define RXD_PIN_APPLICATION         35
 
 #define I2C_MASTER_SCL_IO           19        /*!< gpio number for I2C master clock */
 #define I2C_MASTER_SDA_IO           18        /*!< gpio number for I2C master data  */
 #define I2C_MASTER_NUM              I2C_NUM_0 /*!< I2C port number for master dev */
+
 #define THREAD_PRIORITY_NORMAL      5
 #define THREAD_PRIORITY_LOW         3
 #define THREAD_PRIORITY_HIGH        7
+
 #define HC_SR04_SAMPLE_PERIOD_MS    100
 #define HC_SR04_PIN_ECHO            GPIO_NUM_32
 #define HC_SR04_PIN_TRIG            GPIO_NUM_33
 #define HC_SR04_PIN_FRONT_ECHO      GPIO_NUM_25
 #define HC_SR04_PIN_FRONT_TRIG      GPIO_NUM_26
-#define TIME_ZONE (-3)   //Paramaribo Time
-#define YEAR_BASE (2000) //date in GPS starts from 200
+
+#define TIME_ZONE                   (-3)   //Suriname Time zone
+#define YEAR_BASE                   (2000) //date in GPS starts from 2000
 #define TRIGGER_THREAD_STACK_SIZE   1024
+#define RX_BUF_SIZE                 100
 
 _Static_assert(HC_SR04_SAMPLE_PERIOD_MS > 50, "Sample period too short!");
 
@@ -62,29 +82,45 @@ typedef struct {
 static uint32_t cap_val_begin_of_sample = 0;
 static uint32_t cap_val_end_of_sample = 0;
 uint8_t num_satsRead;
-float pressureRead, temperatureRead,
-        barometric_heightRead, headingRead,
-        PitchRead, rollRead,
-        hcsr04Read, latRead,
-        lonRead, gpsAltRead,
-        speedRead;
+float pressureRead = 0.0,
+        temperatureRead = 0.0,
+        barometric_heightRead = 0.0,
+        headingRead = 0.0,
+        PitchRead = 0.0,
+        rollRead = 0.0,
+        hcsr04Read = 0.0,
+        latRead = 0.0,
+        lonRead = 0.0,
+        gpsAltRead = 0.0,
+        speedRead = 0.0,
+        reference_pitch = 0.0,
+        reference_roll = 94.0,
+        reference_height = 15.0;
+int torqueInPWM = 0,
+    rollCorrectionInPWM = 0,
+    pitchCorrectionInPWM = 0,
+    yawCorrectionInPWM = 0;
+bool kill_motors = true;
 
 gps_t *gps = NULL;
-//gps_t *gps = &gps_holder;       //TODO: Bad code, change when time
 
 static QueueHandle_t cap_queue;
 esp_websocket_client_handle_t client;
 
 calibration_t cal = {
-    .mag_offset = {.x = -31594.468750, .y = 5600.710938, .z = -4488.644531},
+    .mag_offset = {.x = 25.183594, .y = 57.519531, .z = -62.648438},
     .mag_scale = {.x = 1.513449, .y = 1.557811, .z = 1.434039},
-    .accel_offset = {.x = 0.104225, .y = 0.090975, .z = -0.046635},
 
+    .accel_offset = {.x = 0.104225, .y = 0.090975, .z = -0.046635},
     .accel_scale_lo = {.x = 1.062676, .y = 1.047455, .z = 0.998575},
     .accel_scale_hi = {.x = -0.931153, .y = -0.948881, .z = -1.018992},
 
     .gyro_bias_offset = {.x = -2.231220, .y = 0.400365, .z = -0.380849}
 };
+
+//-------------function declaration. TODO: move to header file and make other declarations there
+void assignValues(char *dataString);
+// ------------end of FUNC DECL
 
 /**
  * Transformation:
@@ -120,6 +156,42 @@ static void transform_mag(vector_t *v)
   v->z = -x;
 }
 
+void init_rf_comm(void) {
+    const uart_config_t uart_config = {
+        .baud_rate = 9600,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+    // We won't use a buffer for sending data.
+    uart_driver_install(UART_NUM_2, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_2, &uart_config);
+    uart_set_pin(UART_NUM_2, TXD_PIN_APPLICATION, RXD_PIN_APPLICATION, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+}
+
+static void rx_task(void *arg)
+{
+    init_rf_comm();
+    static const char *RX_TASK_TAG = "RX_TASK";
+    esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
+    uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE+1);
+    char dataStr[10];
+    while (1) {
+        const int rxBytes = uart_read_bytes(UART_NUM_2, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
+        if (rxBytes > 0) {
+            data[rxBytes] = 0;
+            ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
+            sprintf(dataStr, "%s", (char *)data);
+            assignValues(&dataStr);
+            ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
+        }
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+    free(data);
+}
+
 static void websocket_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
     esp_websocket_event_data_t *data = (esp_websocket_event_data_t *)event_data;
@@ -139,6 +211,39 @@ static void websocket_event_handler(void *handler_args, esp_event_base_t base, i
     }
 }
 
+/*
+ * breaks  up string from websocket/RF and assigns important flight variables
+ */
+void assignValues(char *dataString){
+        char *receivedValue;
+        strtok_r(dataString, "-", &receivedValue);
+        uint8_t dataInt = atoi(dataString);
+        if (receivedValue == NULL){
+            return;
+        }
+
+        printf("integer %d\n", dataInt);
+        printf("value in string: %s\n", receivedValue);
+        switch(dataInt){
+            case 0:
+                kill_motors = atoi(receivedValue);
+                //printf("debug in case 00");
+                break;
+            case 1:
+                torqueInPWM = atoi(receivedValue);
+                //printf("debug in case 01");
+                break;
+            case 10:
+                reference_height = atof(receivedValue);
+                break;
+            default:
+                break;
+        }
+        printf("kill %d\n", kill_motors);
+        printf("torqueInPWM: %d\n", torqueInPWM);
+        printf("height referenceede: %f\n", reference_height);
+}
+
 void websocket_setup(){
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
@@ -146,11 +251,13 @@ void websocket_setup(){
     ESP_ERROR_CHECK(example_connect());
 
     esp_websocket_client_config_t websocket_cfg = {};
+
     websocket_cfg.uri = "ws://192.168.1.15:8080";
 
     ESP_LOGI(TAG, "Connecting to %s...", websocket_cfg.uri);
 
     client = esp_websocket_client_init(&websocket_cfg);
+
     esp_websocket_register_events(client, WEBSOCKET_EVENT_ANY, websocket_event_handler, (void *)client);
 
     esp_websocket_client_start(client);
@@ -212,7 +319,7 @@ void post_to_websocket(void *args){
         strcat(str, "}");
 
         if (esp_websocket_client_is_connected(client)) {
-            ESP_LOGI(TAG, "%s\n\n", str);
+            //ESP_LOGI(TAG, "%s\n\n", str);
             esp_websocket_client_send_text(client, str, strlen(str), portMAX_DELAY);
         }
         vTaskDelay(1000 / portTICK_PERIOD_MS);
@@ -309,7 +416,7 @@ void run_hcsr04(){
             continue;
         }
         hcsr04Read = (float) pulse_width_us / 58;
-        ESP_LOGI(TAG, "Pulse width: %uus, Measured distance: %.2fcm", pulse_width_us, hcsr04Read);
+        //ESP_LOGI(TAG, "Pulse width: %uus, Measured distance: %.2fcm", pulse_width_us, hcsr04Read);
     }
 }
 
@@ -332,10 +439,10 @@ void get_altitude_from_pressure(){
 }
 
 void set_bldc_speed(int fr, int fl, int br, int bl){
-    ESP_ERROR_CHECK(mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, fl));
-    ESP_ERROR_CHECK(mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_1, MCPWM_OPR_A, fr));
-    ESP_ERROR_CHECK(mcpwm_set_duty_in_us(MCPWM_UNIT_1, MCPWM_TIMER_0, MCPWM_OPR_A, bl));
-    ESP_ERROR_CHECK(mcpwm_set_duty_in_us(MCPWM_UNIT_1, MCPWM_TIMER_1, MCPWM_OPR_A, br));
+    ESP_ERROR_CHECK(mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, fl + MOTOR_OFFSET_FL));
+    ESP_ERROR_CHECK(mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_1, MCPWM_OPR_A, fr + MOTOR_OFFSET_FR));
+    ESP_ERROR_CHECK(mcpwm_set_duty_in_us(MCPWM_UNIT_1, MCPWM_TIMER_0, MCPWM_OPR_A, bl + MOTOR_OFFSET_BL));
+    ESP_ERROR_CHECK(mcpwm_set_duty_in_us(MCPWM_UNIT_1, MCPWM_TIMER_1, MCPWM_OPR_A, br + MOTOR_OFFSET_BR));
 }
 
 void mcpwm_setup(){
@@ -356,8 +463,7 @@ void mcpwm_setup(){
     mcpwm_init(MCPWM_UNIT_1, MCPWM_TIMER_0, &pwm_config);
     mcpwm_init(MCPWM_UNIT_1, MCPWM_TIMER_1, &pwm_config);
     set_bldc_speed(0, 0 , 0, 0);
-    vTaskDelay(1000);
-
+    //vTaskDelay(500);          TODO: uncomment delay when not in testing mode
 }
 
 /**
@@ -368,8 +474,7 @@ void mcpwm_setup(){
  * @param event_id event id
  * @param event_data event specific arguments
  */
-static void gps_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
-{
+static void gps_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data){
     switch (event_id) {
     case GPS_UPDATE:
         gps = (gps_t *)event_data;
@@ -397,9 +502,6 @@ static void gps_event_handler(void *event_handler_arg, esp_event_base_t event_ba
     }
 }
 
-/*
- * TASKS
- */
 void run_gps(void *args){
     /* NMEA parser configuration */
     nmea_parser_config_t config = NMEA_PARSER_CONFIG_DEFAULT();
@@ -419,8 +521,10 @@ void run_gps(void *args){
     vTaskDelete(NULL);
 }
 
-void run_BMP280(void *args){
-    i2c_master_init(I2C_MASTER_NUM, I2C_MASTER_SDA_IO, I2C_MASTER_SCL_IO);
+void run_sensor(void *args){
+    i2c_mpu9250_init(&cal);
+    MadgwickAHRSinit(SAMPLE_FREQ_Hz, 0.8);
+
     bmp280_params_t params;
     bmp280_init_default_params(&params);
     bmp280_t dev;
@@ -430,31 +534,16 @@ void run_BMP280(void *args){
     printf("BMP280: found %s\n", bme280p ? "BME280" : "BMP280");
 
     uint64_t i = 0;
-    while(true){
-        float humidity;
-        bmp280_read_float(&dev, &temperatureRead, &pressureRead, &humidity);
-        get_altitude_from_pressure();
-        if(i++ % 500 == 0){
-            //ESP_LOGI(TAG, "barometric height: %2.3fm, outdoor Temperature: %2.3f°C", barometric_heightRead, temperatureRead);
-        }
-        //printf("bmp280\n");           //debug; uncomment to see whenever tasks runs
-        pauses();
-    }
-    vTaskDelete(NULL);
-}
-
-void run_sensor(void *args)
-{
-    i2c_mpu9250_init(&cal);
-    MadgwickAHRSinit(SAMPLE_FREQ_Hz, 0.8);
-
-    uint64_t i = 0;
     while (true)
     {
+        float humidity;
+
         vector_t va, vg, vm;
 
         // Get the Accelerometer, Gyroscope and Magnetometer values.
         ESP_ERROR_CHECK(get_accel_gyro_mag(&va, &vg, &vm));
+        bmp280_read_float(&dev, &temperatureRead, &pressureRead, &humidity);
+        get_altitude_from_pressure();
 
         // Transform these values to the orientation of our device.
         transform_accel_gyro(&va);
@@ -505,27 +594,121 @@ static void sensor_task(void *arg)
 }*/
 
 static void run_motors(void *args){
+    int mma1 = 0, mma2 = 0, mma3 = 0, mma4 = 0;
+    float pitchError, pitchCorrection,
+            rollError, rollCorrection,
+            heightError, heightCorrection,
+            reference_pitch = 0.0,
+            reference_roll = 90.0,
+            reference_height = 10.0;
+
+    pid_ctrl_parameter_t pitchParam = {
+        .kp = 0.2,                      //0.8
+        .ki = 0.0,                    //0.002
+        .kd = 0.,                      //0.5
+        .max_output = 50,
+        .min_output = -50,
+        .max_integral = 20,
+        .min_integral = -20,
+        .cal_type = PID_CAL_TYPE_POSITIONAL
+    };
+
+    pid_ctrl_parameter_t rollParam = {
+        .kp = 0.2,                      //0.8
+        .ki = 0.0,                    //0.002
+        .kd = 0.,                      //0.3
+        .max_output = 50,
+        .min_output = -50,
+        .max_integral = 20,
+        .min_integral = -20,
+        .cal_type = PID_CAL_TYPE_POSITIONAL
+    };
+
+    pid_ctrl_parameter_t heightParam = {
+        .kp = 0.011,
+        .ki = 0.002,
+        .kd = 0.3,
+        .max_output = 10,
+        .min_output = -10,
+        .max_integral = 0.05,
+        .min_integral = -0.05,
+        .cal_type = PID_CAL_TYPE_POSITIONAL
+    };
+
+    pid_ctrl_config_t pid_ctrl_config_pitch = {.init_param = pitchParam};
+    pid_ctrl_config_t pid_ctrl_config_roll = {.init_param = rollParam};
+    pid_ctrl_config_t pid_ctrl_config_height = {.init_param = heightParam};
+
+    pid_ctrl_block_handle_t pid_ctrl_block_handle_pitch;
+    pid_ctrl_block_handle_t pid_ctrl_block_handle_roll;
+    pid_ctrl_block_handle_t pid_ctrl_block_handle_height;
+
+    pid_new_control_block(&pid_ctrl_config_pitch, &pid_ctrl_block_handle_pitch);
+    pid_new_control_block(&pid_ctrl_config_roll, &pid_ctrl_block_handle_roll);
+    pid_new_control_block(&pid_ctrl_config_height, &pid_ctrl_block_handle_height);
+
     mcpwm_setup();
+
     while(true){
-        set_bldc_speed(1200, 1200, 1200, 1200);
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        pitchError = PitchRead - reference_pitch;
+        rollError = rollRead - reference_roll;
+        heightError = reference_height - hcsr04Read;;
+
+        if (kill_motors == 1){
+            mma1 = 0, mma2 = 0, mma3 = 0, mma4 = 0;
+            pitchCorrection = 0;
+            rollCorrection = 0;
+            heightCorrection = 0;
+            torqueInPWM = 0;
+            //ESP_LOGI(TAG, "DIE MOTORS, DIEEEEEEEEEE!!!!!!!!\n");
+        } else{
+            pid_compute(pid_ctrl_block_handle_pitch, pitchError, &pitchCorrection);
+            pid_compute(pid_ctrl_block_handle_roll, rollError, &rollCorrection);
+            pid_compute(pid_ctrl_block_handle_height, heightError, &heightCorrection);
+
+            pitchCorrectionInPWM = (int) pitchCorrection;
+            rollCorrectionInPWM = (int) rollCorrection;
+            //torqueInPWM += (int)heightCorrection;
+
+            if(torqueInPWM < 1000){
+                torqueInPWM = 1000;
+            } else if (torqueInPWM > 1700){
+                torqueInPWM = 1700;
+            }
+
+            mma1 = torqueInPWM - rollCorrectionInPWM + pitchCorrectionInPWM + yawCorrectionInPWM;
+            mma2 = torqueInPWM + rollCorrectionInPWM + pitchCorrectionInPWM - yawCorrectionInPWM;
+            mma3 = torqueInPWM - rollCorrectionInPWM - pitchCorrectionInPWM - yawCorrectionInPWM;
+            mma4 = torqueInPWM + rollCorrectionInPWM - pitchCorrectionInPWM + yawCorrectionInPWM;
+        }
+
+        set_bldc_speed(mma1, mma2, mma3, mma4);
 
 
         //printf("mcpwm\n");    //debug; uncomment to see whenever tasks runs
+        //printf("error: %f\n", pitchError);
+        //ESP_LOGI(TAG, "Pitch: %f deg\n", PitchRead);
+        ESP_LOGI(TAG, "HEIGHT CORRECTION: %f \n", heightCorrection);
+        //printf("correctie: %f\n", pitchCorrectionInFloat);
+        printf("pwm value: %d\t %d\t %d\t %d\n", mma1, mma2, mma3, mma4);
+
+        vTaskDelay(10 / portTICK_PERIOD_MS);
     }
     vTaskDelete(NULL);
 }
 
-void app_main(void)
-{
+void app_main(void){
+
+    //vTaskDelay(1000);   //get power level high enough then begin
     websocket_setup();  //gives watchdog error if in websocket task
 
-    xTaskCreate(run_sensor, "MPU9250_task", 2500, NULL, THREAD_PRIORITY_NORMAL, NULL);   //stack overflow if size = 2048
-    xTaskCreate(run_motors, "mcpwm_task", 2048, NULL, THREAD_PRIORITY_NORMAL, NULL);
-    xTaskCreate(run_BMP280, "BMP280_task", 2048, NULL, THREAD_PRIORITY_NORMAL, NULL);
+    xTaskCreate(run_sensor, "MPU9250_task", 4096, NULL, THREAD_PRIORITY_NORMAL, NULL);
+
+    //motor runs on idle priority which is highest priority task can get. make absolutely sure other tasks have sufficient delay
+    xTaskCreate(run_motors, "mcpwm_task", 4096, NULL, tskIDLE_PRIORITY, NULL);
+
     xTaskCreate(run_hcsr04, "HCSR-04_task", 2048, NULL, THREAD_PRIORITY_NORMAL, NULL);
-    xTaskCreate(run_gps, "GPS_task", 2048, NULL, THREAD_PRIORITY_NORMAL, NULL);
-    xTaskCreate(post_to_websocket, "websocket_task", 4096, NULL, THREAD_PRIORITY_NORMAL, NULL);
-
-
+    //xTaskCreate(run_gps, "GPS_task", 2048, NULL, THREAD_PRIORITY_NORMAL, NULL);
+    xTaskCreate(post_to_websocket, "websocket_task", 2500, NULL, THREAD_PRIORITY_NORMAL, NULL);
+    xTaskCreate(rx_task, "flightApp_task", 3000, NULL, THREAD_PRIORITY_NORMAL, NULL);
 }
--- websocketApp/index.js
+++ websocketApp/index.js
@@ -4,21 +4,9 @@ var http = require("http");
 var fs = require("fs");
 
 const wss = new WebSocketServer.Server({ port: 8080 })
+const wss2 = new WebSocketServer.Server({ port: 7050 })
 
 var dataHolder = "nan"
-
-function return_json_from_str(str){
-    const myArray = str.split("-");
-
-    var sensors = {
-        heading: myArray[0],
-        pitch : myArray[1],
-        roll: myArray[2]
-    };
-
-    return sensors;
-}
-
 
 let sockets = [];
 wss.on("connection", ws => {
@@ -26,10 +14,8 @@ wss.on("connection", ws => {
     sockets.push(ws);
 
     ws.on("message", data => {
-        console.log(`Client has sent us: ${data}`)
+        console.log(`${data}`)
         dataHolder = String(data);
-        var foo = data;
-        //jsonStr = return_json_from_str(dataHolder);
         sockets.forEach(s => {
             s.send(dataHolder);
         });
@@ -44,6 +30,29 @@ wss.on("connection", ws => {
     }
 });
 console.log("The WebSocket server is running on port 8080");
+
+let second_sockets = [];
+wss2.on("connection", ws => {
+    console.log("new client connected");
+    second_sockets.push(ws);
+
+    ws.on("message", data => {
+        console.log(`ESP32 has sent us: ${data}`);
+        second_sockets.forEach(s => {
+            s.send(data);
+        });
+    });
+
+    // handling what to do when clients disconnects from server
+    ws.on("close", () => {
+        console.log("the client has disconnected");
+    });
+    // handling client connection error
+    ws.onerror = function () {
+        console.log("Some Error occurred")
+    }
+});
+console.log("The WebSocket server is running on port 7050");
 
 var server = http.createServer(function(request, response) {
     response.writeHead(200, {
